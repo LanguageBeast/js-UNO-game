4ebf674bdc6a1c0c5e1d4c248d003539e0d01fe9
Martin Herrera <genkida_mh01@hotmail.com>
2023-01-14 17:27:21 -0300

    Added wild card mechanic + keeping track of current color

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ const leftPlayerElement = document.querySelector(".player-left");
 const rightPlayerElement = document.querySelector(".player-right");
 const playButtonElement = document.querySelector(".play-button");
 const passTurnButtonElement = document.querySelector(".pass-turn");
+const currentColorSpanElement = document.querySelector(".current-color span");
+const chooseAColorElement = document.querySelector(".choose-color");
+const colorButtonElements = document.querySelectorAll(".choose-color button");
 let allCardsInDeck;
 let cardsInDeck;
 
@@ -24,6 +27,9 @@ const dealDelay = 50;
 const finishedDealing = false;
 let turnFlow = "right";
 let lastSpecialCardPlayed = "";
+
+const colors = ["green", "blue", "red", "yellow"];
+let chosenColor = "";
 
 // objects
 let hasCurrentPlayerDrawnCard;
@@ -104,38 +110,50 @@ const cards = [
   { color: "yellow", type: "number", value: 7 },
   { color: "yellow", type: "number", value: 8 },
   { color: "yellow", type: "number", value: 9 },
-  { color: "red", type: "special", value: "reverse" },
-  { color: "red", type: "special", value: "reverse" },
-  { color: "yellow", type: "special", value: "reverse" },
-  { color: "yellow", type: "special", value: "reverse" },
-  { color: "green", type: "special", value: "reverse" },
-  { color: "green", type: "special", value: "reverse" },
-  { color: "blue", type: "special", value: "reverse" },
-  { color: "blue", type: "special", value: "reverse" },
-  { color: "red", type: "special", value: "skip" },
-  { color: "red", type: "special", value: "skip" },
-  { color: "yellow", type: "special", value: "skip" },
-  { color: "yellow", type: "special", value: "skip" },
-  { color: "green", type: "special", value: "skip" },
-  { color: "green", type: "special", value: "skip" },
-  { color: "blue", type: "special", value: "skip" },
-  { color: "blue", type: "special", value: "skip" },
+  // { color: "red", type: "special", value: "reverse" },
+  // { color: "red", type: "special", value: "reverse" },
+  // { color: "yellow", type: "special", value: "reverse" },
+  // { color: "yellow", type: "special", value: "reverse" },
+  // { color: "green", type: "special", value: "reverse" },
+  // { color: "green", type: "special", value: "reverse" },
+  // { color: "blue", type: "special", value: "reverse" },
+  // { color: "blue", type: "special", value: "reverse" },
+  // { color: "red", type: "special", value: "skip" },
+  // { color: "red", type: "special", value: "skip" },
+  // { color: "yellow", type: "special", value: "skip" },
+  // { color: "yellow", type: "special", value: "skip" },
+  // { color: "green", type: "special", value: "skip" },
+  // { color: "green", type: "special", value: "skip" },
+  // { color: "blue", type: "special", value: "skip" },
+  // { color: "blue", type: "special", value: "skip" },
+  { color: "black-wild", type: "special", value: "wild" },
+  { color: "black-wild", type: "special", value: "wild" },
+  { color: "black-wild", type: "special", value: "wild" },
+  { color: "black-wild", type: "special", value: "wild" }, ///////
   { color: "black-wild", type: "special", value: "wild" },
   { color: "black-wild", type: "special", value: "wild" },
   { color: "black-wild", type: "special", value: "wild" },
   { color: "black-wild", type: "special", value: "wild" },
-  { color: "red", type: "special", value: "draw-two" },
-  { color: "red", type: "special", value: "draw-two" },
-  { color: "yellow", type: "special", value: "draw-two" },
-  { color: "yellow", type: "special", value: "draw-two" },
-  { color: "green", type: "special", value: "draw-two" },
-  { color: "green", type: "special", value: "draw-two" },
-  { color: "blue", type: "special", value: "draw-two" },
-  { color: "blue", type: "special", value: "draw-two" },
-  { color: "four-wild", type: "special", value: "wild-draw-four" },
-  { color: "four-wild", type: "special", value: "wild-draw-four" },
-  { color: "four-wild", type: "special", value: "wild-draw-four" },
-  { color: "four-wild", type: "special", value: "wild-draw-four" },
+  { color: "black-wild", type: "special", value: "wild" },
+  { color: "black-wild", type: "special", value: "wild" },
+  { color: "black-wild", type: "special", value: "wild" },
+  { color: "black-wild", type: "special", value: "wild" },
+  { color: "black-wild", type: "special", value: "wild" },
+  { color: "black-wild", type: "special", value: "wild" },
+  { color: "black-wild", type: "special", value: "wild" },
+  { color: "black-wild", type: "special", value: "wild" },
+  // { color: "red", type: "special", value: "draw-two" },
+  // { color: "red", type: "special", value: "draw-two" },
+  // { color: "yellow", type: "special", value: "draw-two" },
+  // { color: "yellow", type: "special", value: "draw-two" },
+  // { color: "green", type: "special", value: "draw-two" },
+  // { color: "green", type: "special", value: "draw-two" },
+  // { color: "blue", type: "special", value: "draw-two" },
+  // { color: "blue", type: "special", value: "draw-two" },
+  // { color: "four-wild", type: "special", value: "wild-draw-four" },
+  // { color: "four-wild", type: "special", value: "wild-draw-four" },
+  // { color: "four-wild", type: "special", value: "wild-draw-four" },
+  // { color: "four-wild", type: "special", value: "wild-draw-four" },
 ];
 
 // players
@@ -166,6 +184,12 @@ generateCards();
 playButtonElement.addEventListener("click", () => {
   startGame();
 });
+// add event listener to color buttons
+colorButtonElements.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    handleColorChoosing(e.target.id);
+  });
+});
 
 // ------------------------------
 
@@ -184,12 +208,15 @@ function startGame() {
 }
 // make the computer play
 function nextPlay() {
-  togglePlayerOPPlayability();
+  if (bottomPlayerElement.classList.contains("clickable")) {
+    togglePlayerOPPlayability();
+  }
   hasCurrentPlayerDrawnCard = false;
   if (
     lastSpecialCardPlayed === "skip" ||
     lastSpecialCardPlayed === "reverse" ||
-    lastSpecialCardPlayed === "draw-two"
+    lastSpecialCardPlayed === "draw-two" ||
+    lastSpecialCardPlayed === "wild"
   ) {
     handleSpecialCard(currentTurn);
   } else {
@@ -219,6 +246,7 @@ function makeBotPlay(usedTurn) {
     }
   }
   if (chosenRandomCard) {
+    chosenColor = "";
     setTimeout(flipCard, 500, chosenRandomCard);
     setTimeout(dispatchCardToDiscardPile, 1000, chosenRandomCard, usedTurn);
   }
@@ -446,8 +474,12 @@ function flipMainPlayerCards() {
 // start discard pile
 function startDiscardPile() {
   let cardElement = extractCardFromDeck();
+  dispatchCardToDiscardPile;
   addChildElement(discardPileElement, cardElement);
-  setTimeout(flipCard, 50, cardElement);
+  setTimeout(() => {
+    flipCard(cardElement);
+    updateCurrentColor(cardElement.id);
+  }, 50);
 }
 
 // add clicking functionality to main player cards
@@ -455,6 +487,7 @@ function playCard(card) {
   let chosenCard = getChosenCard(card);
   if (checkIfCardIsPlayable(chosenCard.id)) {
     dispatchCardToDiscardPile(chosenCard);
+    chosenColor = "";
     setTimeout(() => {
       handleNextTurn();
       nextPlay();
@@ -479,6 +512,7 @@ function dispatchCardToDiscardPile(cardElement, setTurn = undefined) {
     updateCardList(removedCard.id, setTurn);
     removedCard.classList.toggle("in-deck");
     checkIfSpecial(removedCard.id);
+    updateCurrentColor(removedCard.id);
     addChildElement(discardPileElement, removedCard);
     setTimeout(() => {
       removedCard.classList.toggle("remove");
@@ -501,23 +535,32 @@ function checkIfCardIsPlayable(cardId) {
   let binCardIds = topcardDiscardPileId.split(" ");
   let chosenCardIds = cardId.split(" ");
   // first come the color, then the type, then the value/action
-  if (binCardIds[0] === "black-wild" || binCardIds[0] === "four-wild") {
-    // if a wild or wild draw four is atop the discard pile, any card card can be used (CHANGE THIS LATER!)
-    return true;
-  } else if (
-    chosenCardIds[2] === "wild" ||
-    chosenCardIds[2] === "wild-draw-four"
-  ) {
-    return true; // wilds or wild draw four can be used everywhere
-  } else if (binCardIds[0] === chosenCardIds[0]) {
-    return true; // both are the same color
-  } else if (
-    binCardIds[1] === chosenCardIds[1] &&
-    binCardIds[2] === chosenCardIds[2]
-  ) {
-    return true; // both are numbers or specials and they're the same
+  if (chosenColor) {
+    // a wild or wild draw four has been played
+    if (chosenCardIds[2] === "wild" || chosenCardIds[2] === "wild-draw-four") {
+      // another wild overrides previous wild!
+      return true;
+    } else if (chosenCardIds[0] === chosenColor) {
+      // card is the same color as the chosen by the wild
+      return true;
+    } else {
+      // card is not compatible
+      return false;
+    }
   } else {
-    return false; // the cards are incompatible
+    // no wild has been played
+    if (chosenCardIds[2] === "wild" || chosenCardIds[2] === "wild-draw-four") {
+      return true; // wilds or wild draw four can be used everywhere
+    } else if (binCardIds[0] === chosenCardIds[0]) {
+      return true; // both are the same color
+    } else if (
+      binCardIds[1] === chosenCardIds[1] &&
+      binCardIds[2] === chosenCardIds[2]
+    ) {
+      return true; // both are numbers or specials and they're the same
+    } else {
+      return false; // the cards are incompatible
+    }
   }
 }
 
@@ -595,9 +638,10 @@ function handleSpecialCard(usedTurn) {
     case "reverse":
       handleReverseCard();
       break;
-    // case "wild":
-    //   handleWildCard();
-    //   break;
+    case "wild":
+      console.log("wild");
+      handleWildCard(usedTurn);
+      break;
     case "draw-two":
       handleDrawTwoCard(usedTurn);
       break;
@@ -625,6 +669,59 @@ function handleDrawTwoCard(usedTurn) {
   }
   handleNextTurn();
   lastSpecialCardPlayed = "";
+  setTimeout(nextPlay, 500);
+}
+function handleWildCard(usedTurn) {
+  if (usedTurn === 1) {
+    // main player
+    togglePlayerOPPlayability();
+    toggleColorSelect();
+  } else {
+    // randomize color (maybe i'll change it later!)
+    let randomColor = colors[Math.floor(Math.random() * colors.length)];
+    updateCurrentColor(randomColor);
+    chosenColor = randomColor;
+    setTimeout(nextPlay, 500);
+  }
+  lastSpecialCardPlayed = "";
+}
+
+// handle current color and color choosing
+function updateCurrentColor(cardId) {
+  const cardParts = cardId.split(" ");
+  const color = cardParts[0];
+  switch (color) {
+    case "red":
+      handleColorChange("red-text", "Red");
+      break;
+    case "green":
+      handleColorChange("green-text", "Green");
+      break;
+    case "blue":
+      handleColorChange("blue-text", "Blue");
+      break;
+    case "yellow":
+      handleColorChange("yellow-text", "Yellow");
+      break;
+    case "four-wild" || "black-wild":
+      handleColorChange("", "Wild card!");
+      break;
+  }
+}
+function handleColorChange(colorClass, value = "") {
+  currentColorSpanElement.classList = "";
+  currentColorSpanElement.classList += colorClass;
+  currentColorSpanElement.innerText = value;
+}
+function toggleColorSelect() {
+  chooseAColorElement.classList.toggle("choose-color-invisible");
+}
+function handleColorChoosing(colorId) {
+  chosenColor = colorId;
+  setTimeout(() => {
+    updateCurrentColor(colorId);
+    toggleColorSelect();
+  }, 250);
   setTimeout(nextPlay, 500);
 }
 // utility functions
